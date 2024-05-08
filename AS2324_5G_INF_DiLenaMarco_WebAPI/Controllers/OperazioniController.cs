72b9126440d932926e6d8d24327b6bd57af13819
Marco Di Lena <marco.dilena@studenti.ittsrimini.edu.it>
2024-05-08 08:27:24 +0200

    feat:aggiunta metodo del multiplo

--- AS2324_5G_INF_DiLenaMarco_WebAPI/Controllers/OperazioniController.cs
+++ AS2324_5G_INF_DiLenaMarco_WebAPI/Controllers/OperazioniController.cs
@@ -7,5 +7,19 @@ namespace AS2324_5G_INF_DiLenaMarco_WebAPI.Controllers
     [ApiController]
     public class OperazioniController : ControllerBase
     {
+        [HttpGet]
+        public JsonResult Moltiplicatore(int num, int factor)
+        {
+            string res;
+            if (num < factor && factor/num < factor )
+            {
+                res = "il primo numero non è moltiplicatore del secondo";
+            }
+            else
+            {
+                res = "il primo numero è moltiplicatore del secondo";
+            }
+            return new JsonResult(new JsonResult(res));
+        }
     }
 }
