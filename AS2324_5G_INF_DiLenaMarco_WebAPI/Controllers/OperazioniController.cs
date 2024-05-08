affeeca7b7f668ca96c4cdd7535f28fc1ec05f0e
Marco Di Lena <marco.dilena@studenti.ittsrimini.edu.it>
2024-05-08 08:11:51 +0200

    create:cartella models e controller API

---
+++ AS2324_5G_INF_DiLenaMarco_WebAPI/Controllers/OperazioniController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AS2324_5G_INF_DiLenaMarco_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperazioniController : ControllerBase
+    {
+    }
+}
